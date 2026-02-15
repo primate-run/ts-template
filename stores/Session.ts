ecb62ac9d889bf44603444260b2fd2d3561f2f2b
terrablue <102580937+terrablue@users.noreply.github.com>
2026-02-15 23:54:30 +0100

    sync

--- stores/Session.ts
+++ stores/Session.ts
@@ -1,8 +1,9 @@
-import p from "primate/pema";
-import store from "primate/store";
+import p from "pema";
+import key from "primate/orm/key";
+import store from "primate/orm/store";
 
 export default store({
-  id: p.primary,
+  id: key.primary(p.u32),
   session_id: p.string.uuid(),
   foo: p.string,
 });
